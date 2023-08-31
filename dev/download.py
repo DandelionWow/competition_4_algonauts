2499786c3a3a8cdec2008fb90963200cd9edba44
sunyang <young7869264s@163.com>
2023-08-31 15:51:02 +0800

    🐞 fix(dev): 下载文件修改
    由于条件限制，修改下载思路，可分两阶段下载（先抓取url，再下载data）

--- dev/download.py
+++ dev/download.py
@@ -24,81 +24,94 @@ class Chrome:
         # 发请求
         self.driver.get(self.base_url if uri == '' else self.base_url + '#' + uri) 
 
-    def download_data(self, cur_dir: str=''):
-        # 睡一会
-        time.sleep(1.5)
-        # 当前路径
-        cur_path = os.path.join(self.dataset_prefix_path, cur_dir)
-        os.makedirs(cur_path, exist_ok=True) # 若不存在，则创建
-        print('=======目标路径=======')
-        print(cur_path)
-        
-        # 找到分页，遍历所有page
-        pagination_list = self.driver.find_elements(By.CSS_SELECTOR, ".pagination li")
-        for i, _ in enumerate(pagination_list):
-            # 找到文件/文件夹列表
-            s3_objects = self.driver.find_element(By.ID, "tbody-s3objects")
-            folders_or_objects = s3_objects.find_elements(By.CSS_SELECTOR, 'a')
-            # 遍历
-            for f_or_o in folders_or_objects:
-                attr_data_s3 = f_or_o.get_attribute('data-s3')
-                if attr_data_s3 == 'folder': # 文件夹，需要递归调用
-                    attr_data_prefix = f_or_o.get_attribute('data-prefix')
-                    # 跳过已下载的文件夹
-                    if attr_data_prefix in skip_img_set:
-                        continue
+def http_get(url):
+    # 随机生成User-Agent头信息，用于反爬虫
+    ua = UserAgent()
+    headers = {'User-Agent': ua.random}
+    # get请求，自动跳转，添加头信息
+    try:
+        # 绕过代理
+        proxies = {'http': None, 'https': None}
+        res = requests.get(url, allow_redirects=True, headers=headers, verify=False, timeout=20., proxies=proxies)
+    except requests.exceptions.ConnectionError:
+        return None
+    return res
 
-                    # 递归文件夹内的内容
-                    chrome = Chrome(args.proxy, args.chromedriver_path, args.parent_path, args.base_url)
-                    chrome.get(attr_data_prefix)
-                    chrome.download_data(attr_data_prefix)
-                elif attr_data_s3 == 'object': # 文件，直接下载
-                    time.sleep(random.randint(1, 2))
-                    attr_href = f_or_o.get_attribute('href') # 文件下载链接
-                    attr_download = f_or_o.get_attribute('download') # 当作文件名
-                    # 跳过已下载
-                    if attr_href in skip_img_set:
-                        continue
+def get_data_url(chrome: Chrome, cur_dir: str = ''):
+    # 睡一会
+    time.sleep(1.5)
+    # 当前路径
+    cur_path = chrome.dataset_prefix_path + '/' + cur_dir
+    print('=======目标路径=======')
+    print(cur_path)
+    
+    # 找到分页，遍历所有page
+    pagination_list = chrome.driver.find_elements(By.CSS_SELECTOR, ".pagination li")
+    for i, _ in enumerate(pagination_list):
+        # 找到文件/文件夹列表
+        s3_objects = chrome.driver.find_element(By.ID, "tbody-s3objects")
+        folders_or_objects = s3_objects.find_elements(By.CSS_SELECTOR, 'a')
+        # 遍历
+        for f_or_o in folders_or_objects:
+            attr_data_s3 = f_or_o.get_attribute('data-s3')
+            if attr_data_s3 == 'folder': # 文件夹，需要递归调用
+                attr_data_prefix = f_or_o.get_attribute('data-prefix')
+                
+                # 递归文件夹内的内容
+                chrome_ = Chrome(args.proxy, args.chromedriver_path, args.parent_path, args.base_url)
+                chrome_.get(attr_data_prefix)
+                get_data_url(chrome_, attr_data_prefix)
+            elif attr_data_s3 == 'object': # 文件，直接下载
+                time.sleep(random.randint(1, 2))
+                attr_href = f_or_o.get_attribute('href') # 文件下载链接
+                attr_download = f_or_o.get_attribute('download') # 当作文件名
 
-                    # 下载
-                    object = self.http_get(attr_href)
-                    if object is None:
-                        print('+++下载失败+++ ---> '+attr_href)
-                    else:
-                        # 保存
-                        object_path = os.path.join(cur_path, attr_download)
-                        with open(object_path, 'wb') as f:
-                            f.write(object.content)
-                            f.flush()
-                            f.close()
-                            # 保存下载记录
-                            skip_file.write(self.driver.current_url.split('#')[1] + '\n')
-                            skip_file.write(attr_href + '\n')
-                            skip_file.flush()
-                            print('+++下载成功+++ ---> '+attr_href)
-            # 若只有1页是3个元素
-            if (i + 3) >= len(pagination_list):
-                break
-            # 下一页
-            time.sleep(1.8)
-            pagination_list = self.driver.find_elements(By.CSS_SELECTOR, ".pagination li") # 再定位元素，防止StaleElementReferenceException
-            pagination_list[len(pagination_list) - 1].find_element(By.CSS_SELECTOR, 'a').click()
-            time.sleep(1.3)
-        # 关闭浏览器
-        self.driver.quit()
+                # 保存url
+                obj = {
+                    'dir': cur_path, 
+                    'file_path': cur_path + attr_download, 
+                    'url': attr_href,
+                }
+                line = json.dumps(obj) + '\n'
+                data_url_file.write(line)
+                data_url_file.flush()
+                
+        # 若只有1页是3个元素
+        if (i + 3) >= len(pagination_list):
+            break
+        # 下一页
+        time.sleep(1.8)
+        pagination_list = chrome.driver.find_elements(By.CSS_SELECTOR, ".pagination li") # 再定位元素，防止StaleElementReferenceException
+        pagination_list[len(pagination_list) - 1].find_element(By.CSS_SELECTOR, 'a').click()
+        time.sleep(1.3)
+    # 关闭浏览器
+    chrome.driver.quit()
 
-    def http_get(self, url):
-        # 随机生成User-Agent头信息，用于反爬虫
-        ua = UserAgent()
-        headers = {'User-Agent': ua.random}
-        # get请求，自动跳转，添加头信息
-        try:
-            # 绕过代理
-            proxies = {'http': None, 'https': None}
-            res = requests.get(url, allow_redirects=True, headers=headers, verify=False, timeout=20., proxies=proxies)
-        except requests.exceptions.ConnectionError:
-            return None
-        return res
+def download_data(data_url_file, skip_file_set):
+    data_url_file.seek(0)
+    data_url_set = set(data_url_file.read().splitlines())
+
+    for obj in data_url_set:
+        obj: dict = json.loads(obj)
+        # 跳过已下载
+        if obj['url'] in skip_file_set:
+            continue
+        # 创建文件夹
+        os.makedirs(obj['dir'], exist_ok=True)
+        # 下载文件
+        data = http_get(obj['url'])
+        if data is None:
+            print('+++下载失败+++ ---> ' + obj['url'])
+        else:
+            # 保存
+            with open(obj['file_path'], 'wb') as f:
+                f.write(data.content)
+                f.flush()
+                f.close()
+                # 保存下载记录
+                skip_file.write(obj['url'] + '\n')
+                skip_file.flush()
+                print('+++下载成功+++ ---> ' + obj['url'])
 
 if __name__ == '__main__':
     # paremeters
@@ -111,17 +124,23 @@ if __name__ == '__main__':
 
     # path设置
     os.makedirs(args.parent_path, exist_ok=True)
+    # 保存下载链接文件 保存格式(每行)：{'dir': '', 'file_path': '', 'url': ''}
+    data_url_file_path = os.path.join(args.parent_path, 'data_url_file')
+    data_url_file = open(data_url_file_path, 'a+')
     # 跳过文件
     skip_file_path = os.path.join(args.parent_path, 'skip_file')
     skip_file = open(skip_file_path, 'a+')
     skip_file.seek(0) # 光标移到文件开始
-    skip_img_set = set(skip_file.read().splitlines()) # 记录：uri
+    skip_file_set = set(skip_file.read().splitlines()) # 记录：uri
 
     # 初始化浏览器
     chrome = Chrome(args.proxy, args.chromedriver_path, args.parent_path, args.base_url)
-    # 打开网页
+    # 打开网页首页
     chrome.get('')
+    # 获取所有数据url
+    get_data_url(chrome)
+
     # 下载数据
-    chrome.download_data()
+    download_data(data_url_file, skip_file_set)
 
     print('++++下载完成++++')
